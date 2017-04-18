1baf001779298efdf3ef34789d0ee5230001ae20
manu <mbinici@fs04.ee.hm.edu>
2017-04-18 03:06:20 +0200

    bug fixes

--- SkriptNet/settings.py
+++ SkriptNet/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static_root/static'
+STATIC_ROOT = 'static_root'
 DOCS_ROOT = os.path.join(BASE_DIR, 'docs/_build/html')
 
 LOGIN_URL = '/'
