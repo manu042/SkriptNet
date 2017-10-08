a454fd0b23dc6b03bb4387422a1d0d391c218196
manu <mbinici@fs04.ee.hm.edu>
2017-10-08 18:51:16 +0200

    imported package

--- skripten_shop/models/models_warehouse.py
+++ skripten_shop/models/models_warehouse.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 # My Packages
-
+from skripten_shop.models import Article
 
 class AritcleInStock(models.Model):
     """
